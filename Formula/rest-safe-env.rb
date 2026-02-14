7bc79b179cf9de396199406d0dd1a7f024fb33ed
Alexandru-Daniel Popescu <ad.popescu338@yahoo.com>
2026-02-15 01:00:14 +0200

    rest-safe-env 0.1.0

---
+++ Formula/rest-safe-env.rb
@@ -0,0 +1,19 @@
+class RestSafeEnv < Formula
+  desc "Encrypt selected .env values at rest and unlock only with explicit local approval."
+  homepage "https://github.com/adpopescu338/rest-safe-env"
+  url "https://registry.npmjs.org/rest-safe-env/-/rest-safe-env-0.1.0.tgz"
+  sha256 "c0f52cc320339af31c77ef95057f9a85f6eaeac531ec64a65b5c3eaf789dac1e"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    libexec.install Dir["package/*"]
+    (bin/"rse").write_exec_script libexec/"bin/rse.js"
+  end
+
+  test do
+    output = shell_output("#{bin}/rse 2>&1", 1)
+    assert_match "Usage:", output
+  end
+end
